009d015fa0cac98492eac1f934686df2c8bd9c3e
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-08-12 13:07:13 +0300

    test23

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { days } from "../../utils/dateUtils.js";
+import moment from "moment";
+import classNames from "classnames";
 
 const Navigation = ({ weekDates }) => (
   <header className="calendar__header">
-    {weekDates.map((dayDate) => (
-      <div className="calendar__day-label day-label">
-        <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
-        <span className="day-label__day-number">{dayDate.getDate()}</span>
-      </div>
-    ))}
+    {weekDates.map((dayDate) => {
+      const isToday =
+        moment(new Date()).format("MMMM DD YYYY") ===
+        moment(dayDate).format("MMMM DD YYYY");
+
+      const dayNameClasses = classNames("day-label__day-name", {
+        "current-name": isToday,
+      });
+      const dayNumberClasses = classNames("day-label__day-number", {
+        "current-number": isToday,
+      });
+      return (
+        <div key={dayDate.getDate()} className="calendar__day-label day-label">
+          <span className={dayNameClasses}>{days[dayDate.getDay()]}</span>
+          <span className={dayNumberClasses}>{dayDate.getDate()}</span>
+        </div>
+      );
+    })}
   </header>
 );
 
